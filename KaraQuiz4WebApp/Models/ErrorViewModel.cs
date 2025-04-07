d43de62f6cfe2795821211a923f0cf76c4bd7a8c
karan-handa-clg <khanda4@my.centennialcollege.ca>
2025-04-07 12:55:30 -0400

    Clean commit from KaraQuiz4WebApp after full reset

---
+++ KaraQuiz4WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KaraQuiz4WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
